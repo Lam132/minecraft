4757543607807a70e48713b9b439ea7701b56d4a
Lam132 <lamdz1311@gmail.com>
2024-01-06 08:49:13 +0700

    minecraft

---
+++ assets/javascript/funct.js
@@ -0,0 +1,26 @@
+
+                let img = document.querySelector('#featurepic');
+                let btn1 = document.querySelector("#pic1");
+                let btn2 = document.querySelector("#pic2");
+                let btn3 = document.querySelector("#pic3");
+                let btn4 = document.querySelector("#pic4");
+                let btn5 = document.querySelector("#pic5");
+                let btn6 = document.querySelector("#pic6");
+                btn1.addEventListener('click', () =>{
+                    img.src = "https://cdn.shopify.com/s/files/1/0266/4841/2351/products/MCACCUTIME-MIN4045-1_cef4dee4-8ef5-4793-813a-cf52f41765c2_900x.png?v=1643228693"
+                })
+                btn2.addEventListener('click', () =>{
+                    img.src = "https://cdn.shopify.com/s/files/1/0266/4841/2351/products/MCACCUTIME-MIN4045-6_083b46ea-e221-40dd-8134-f97cb392ad40_900x.png?v=1643228693"
+                })
+                btn3.addEventListener('click', () =>{
+                    img.src = "https://cdn.shopify.com/s/files/1/0266/4841/2351/products/MCACCUTIME-MIN4045-2_8b88cbae-2ad2-4814-9a37-09f79248c649_900x.png?v=1643222257"
+                })
+                btn4.addEventListener('click', () =>{
+                    img.src = "https://cdn.shopify.com/s/files/1/0266/4841/2351/products/MCACCUTIME-MIN4045-4_3ff4dd3c-0cdb-43fa-881f-10d070e01345_900x.png?v=1643222257"
+                })
+                btn5.addEventListener('click', () =>{
+                    img.src = "https://cdn.shopify.com/s/files/1/0266/4841/2351/products/MCACCUTIME-MIN4045-3_a1886d3c-6721-44f7-8027-c8e1eefaeda2_900x.png?v=1643222257"
+                })
+                btn6.addEventListener('click', () =>{
+                    img.src = "https://cdn.shopify.com/s/files/1/0266/4841/2351/products/MCACCUTIME-MIN4045-5_7ac7fb39-12b3-4556-a715-ea9609381df7_900x.png?v=1643222258"
+                })
